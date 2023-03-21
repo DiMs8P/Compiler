74478f15c6dfda040bc0741a8ba44b24d5669e69
DiMs8P <36597472+DiMs8P@users.noreply.github.com>
2023-03-22 00:30:11 +0700

    added exception handling

---
+++ Compiler/Compiler/ExceptionManager/ExceptionManager.cpp
@@ -0,0 +1,15 @@
+﻿#include "ExceptionManager.h"
+#include <fstream>
+
+void ExceptionManager::Exception(const std::string& errorMessage)
+{
+    std::ofstream errorStream;
+    errorStream.open (_outputFileName);
+    errorStream << errorMessage;
+    errorStream.close();
+}
+
+void ExceptionManager::SetOutputFileName(std::string& newFileName)
+{
+    _outputFileName.swap(newFileName);
+}
---
+++ Compiler/Compiler/ExceptionManager/ExceptionManager.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <string>
+
+class ExceptionManager
+{
+public:
+    static void Exception(const std::string& errorMessage);
+    static void SetOutputFileName(std::string& newFileName);
+
+private:
+    static std::string _outputFileName = "errors.txt";
+};
